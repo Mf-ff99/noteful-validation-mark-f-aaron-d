bff73edf9923981f7e9a03222e878fe3f06ba6c5
Mark Force <force.j.mark@gmail.com>
2020-09-03 13:26:38 -0400

    fixed context

--- src/ApiContext.js
+++ src/ApiContext.js
@@ -3,7 +3,6 @@ import React from 'react'
 export default React.createContext({
   notes: [],
   folders: [],
-  generateFolderId: () => {},
   addFolder: () => {},
   addNote: () => {},
   deleteNote: () => {},
